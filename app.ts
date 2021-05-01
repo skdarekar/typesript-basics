c626c6d5db46e5600c9ec5312633b881f22c85f7
Sagar Darekar <sagardarekar1997@gmail.com>
2021-05-01 13:05:29 +0530

    used type aliases

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
+type Combineable = number | string;
+type ConversionDescriptor = "as-number" | "as-string";
+
 //using unions in ts
 //we can define type as literals => in this case resulttype can only contain 2 values defined
 function combine(
-    input1: number | string,
-    input2: number | string,
-    resultType: "as-number" | "as-string") {
+    input1: Combineable,
+    input2: Combineable,
+    resultType: ConversionDescriptor) {
     let result;
     if (typeof input1 === "number" && typeof input2 === "number" || resultType === "as-number") {
         result = +input1 + +input2;
